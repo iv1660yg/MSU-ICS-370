af0eceec09fb02a919227b0433a20fd5280840ae
iv1660yg <iv1660yg@metrostate.edu>
2019-07-25 00:48:38 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'crsdbuser';
-$DATABASE_PASS = '##12345';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
 $DATABASE_NAME = 'mycsrdb';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
