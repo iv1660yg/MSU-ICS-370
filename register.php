bc895043ca0f09677352f7f755d4ad2c5cad3482
iv1660yg <iv1660yg@metrostate.edu>
2019-07-26 21:16:49 -0500

    Update register.php

--- register.php
+++ register.php
@@ -3,10 +3,21 @@
 $DATABASE_HOST = '127.0.0.1:49399';
 $DATABASE_USER = 'azure';
 $DATABASE_PASS = '6#vWHD_$';
-$DATABASE_NAME = 'localdb';
+$DATABASE_NAME = 'mycrsdb';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	// If there is an error with the connection, stop the script and display the error.
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Now we check if the data was submitted, isset() function will check if the data exists.
+if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+	// Could not get the data that should have been sent.
+	die ('Please complete the registration form!');
+}
+// Make sure the submitted registration values are not empty.
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+	// One or more values are empty.
+	die ('Please complete the registration form');
 }
