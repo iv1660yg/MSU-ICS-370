ecca0aa06fa6fa1022e6972048b644a66c39980e
iv1660yg <iv1660yg@metrostate.edu>
2019-07-27 00:15:15 -0500

    update

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
-<?php
-session_start();
+<?php 
+ob_start();
 include_once("db_connect.php");
+session_start();
 if(isset($_SESSION['user_id'])!="") {
 	header("Location: index.php");
 }
@@ -10,7 +11,7 @@ if (isset($_POST['login'])) {
 	$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email. "' and pass = '" . md5($password). "'");
 	if ($row = mysqli_fetch_array($result)) {
 		$_SESSION['user_id'] = $row['uid'];
-		$_SESSION['user_name'] = $row['user'];
+		$_SESSION['user_name'] = $row['user'];		
 		header("Location: index.php");
 	} else {
 		$error_message = "Incorrect Email or Password!!!";
