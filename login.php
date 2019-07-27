d90545189d993704d250bed0de1a014dc787a8f7
iv1660yg <iv1660yg@metrostate.edu>
2019-07-26 23:56:09 -0500

    update all

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<?php
+session_start();
+include_once("db_connect.php");
+if(isset($_SESSION['user_id'])!="") {
+	header("Location: index.php");
+}
+if (isset($_POST['login'])) {
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email. "' and pass = '" . md5($password). "'");
+	if ($row = mysqli_fetch_array($result)) {
+		$_SESSION['user_id'] = $row['uid'];
+		$_SESSION['user_name'] = $row['user'];
+		header("Location: index.php");
+	} else {
+		$error_message = "Incorrect Email or Password!!!";
+	}
+}
+?>
+
+
+
 <div class="container">
 	<h2>ICS 370 Group Project</h2>		
 	<div class="row">
@@ -29,23 +51,4 @@
 </div>
 
 
-<?php
-session_start();
-include_once("db_connect.php");
-if(isset($_SESSION['user_id'])!="") {
-	header("Location: index.php");
-}
-if (isset($_POST['login'])) {
-	$email = mysqli_real_escape_string($conn, $_POST['email']);
-	$password = mysqli_real_escape_string($conn, $_POST['password']);
-	$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email. "' and pass = '" . md5($password). "'");
-	if ($row = mysqli_fetch_array($result)) {
-		$_SESSION['user_id'] = $row['uid'];
-		$_SESSION['user_name'] = $row['user'];
-		header("Location: index.php");
-	} else {
-		$error_message = "Incorrect Email or Password!!!";
-	}
-}
-?>
 
