94ea1f0bbcc127ed52b5cdae71e8025bf1564adb
iv1660yg <iv1660yg@metrostate.edu>
2019-07-28 23:15:02 -0700

    image

--- register.php
+++ register.php
@@ -30,7 +30,7 @@ if (isset($_POST['signup'])) {
 	}
 	if (!$error) {
 		if(mysqli_query($conn, "INSERT INTO users(user, email, pass) VALUES('" . $name . "', '" . $email . "', '" . md5($password) . "')")) {
-			// $success_message = "Successfully Registered! <a href='login.php'>Click here to Login</a>";
+
 			header("Location:login.php");
 			exit();
 
---
+++ success.php
@@ -0,0 +1,17 @@
+
+
+<div class="container">
+	<h2 align=center>ICS Group Project - Car Rental System</h2>		
+	<div class="row">
+	<img align="center" src="img/checked.png" alt="">
+	<center>Successfully Registered! <a href='login.php'>Click here to Login</a>" </center>
+
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 text-center">	
+		</div>
+	</div>
+		
+	
+</div>
+<?php include('footer.php');?> 
