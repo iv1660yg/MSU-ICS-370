3917ea8df9a2bd8be8e14482c58739833aabf3f8
iv1660yg <iv1660yg@metrostate.edu>
2019-08-10 00:34:58 -0500

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if (isset($_POST['login'])) {
 	$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email. "' and pass = '" . md5($password). "'");
 	if ($row = mysqli_fetch_array($result)) {
 		$_SESSION['user_id'] = $row['uid'];
-		$_SESSION['user_name'] = $row['fullaname'];	
+		$_SESSION['user_name'] = $row['fullname'];	
 		$_SESSION['account_type'] = $row['account_type'];	
 		header("Location: index.php");
 	} else {
