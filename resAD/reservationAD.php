93a4c28fa7ff021ecc7745cb4c5eb6c1ba263d6f
iv1660yg <iv1660yg@metrostate.edu>
2019-08-13 19:54:53 -0500

    Update reservationAD.php

--- resAD/reservationAD.php
+++ resAD/reservationAD.php
@@ -12,7 +12,7 @@ include_once("../db_connect.php");
 		<br>
 		<div class="collapse navbar-collapse" id="navbar1">
 			<ul class="nav navbar-nav navbar-left">
-				<?php if ((isset($_SESSION['user_id']) AND ((($_SESSION['account_type'])=="admin" ) OR (($_SESSION['account_type'])=="clerk" )      ) )) { ?>
+				<?php if ((isset($_SESSION['user_id']) AND (($_SESSION['account_type'])=="admin" ))) { ?>
 				<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['user_name']; ?></strong></p></li>
 				<li><a href="../users/users.php">Manage Users/Customers</a></li>
 				<li><a href="reservationAD.php">Manage Existing Reservations</a></li>
@@ -29,6 +29,16 @@ include_once("../db_connect.php");
 				<li><a href="../reservation.php">Make New Reservation</a></li>
 				<li><a href="../profile.php">Manage Account</a></li>
 				<li><a href="../logout.php">Log Out</a></li>
+				<?php } elseif ((isset($_SESSION['user_id']) AND (($_SESSION['account_type'])=="clerk" ))) { ?>
+					<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['user_name']; ?></strong></p></li>
+				<li><a href="../users/users.php">Manage Users/Customers</a></li>
+				<li><a href="reservationAD.php">Manage Existing Reservations</a></li>
+				<p>
+
+				<?php 
+				require('index.php');
+				?> 
+
 				<?php } else { ?>
 				<li><a href="../login.php">Login</a></li>
 				<li><a href="../register.php">Sign Up</a></li>
